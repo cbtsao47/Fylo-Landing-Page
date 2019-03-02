4a8a35f8ff45d91f8f44a0b68f7edffc014a5fd0
wisdomtooth <cbtsao47@gmail.com>
2019-03-02 02:04:49 -0600

    add menu modal

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.querySelector("");
+const hamburger = document.querySelector(".hamburger");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".modal__close");
+console.log(modal);
+function openMenu(event) {
+  modal.classList.add("show");
+}
+function closeMenu(event) {
+  modal.classList.remove("show");
+}
+hamburger.addEventListener("click", openMenu);
+close.addEventListener("click", closeMenu);
